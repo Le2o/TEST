49553207ca6fb853eca07ee52b912205586ff176
Alexandr <le2o1995@gmail.com>
2019-04-21 22:32:54 +0700

    йцук

---
+++ Tec/Language.cpp
@@ -0,0 +1,55 @@
+#include "Language.h"
+
+Filippov::Language* Filippov::Language_Input(ifstream &fin)
+{
+	Language *language = new Language;
+	Procedural *proc;
+	Object_oriented *oop;
+	Functional *func;
+	unsigned short int temp;
+	fin >> temp;
+	fin >> language->year_of_development;
+	switch (temp)
+	{
+	case 1:
+		proc = new Procedural;
+		language->key = Language::lang::PROCEDURAL;
+		proc = (Procedural *)language;
+		language = (Language *)Procedural_Input(*proc, fin);
+		return language;
+	case 2:
+		oop = new Object_oriented;
+		language->key = Language::lang::OOP;
+		oop = (Object_oriented *)language;
+		language = (Language *)Object_oriented_Input(*oop, fin);
+		return language;
+	case 3:
+		func = new Functional;
+		language->key = Language::lang::FUNCTIONAL;
+		func = (Functional *)language;
+		language = (Language *)Functional_Input(*func, fin);
+		return language;
+	default:
+		return 0;
+	}
+}
+
+void Filippov::Language_Output(Language *obj, ofstream &fout)
+{
+	switch (obj->key)
+	{
+	case Language::lang::PROCEDURAL:
+		Procedural_Output((Procedural*)obj, fout);
+		break;
+	case Language::lang::OOP:
+		Object_oriented_Output((Object_oriented*)obj, fout);
+		break;
+	case Language::lang::FUNCTIONAL:
+		Functional_Output((Functional*)obj, fout);
+		break;
+	default:
+		fout << "Incorrect programming language" << endl;
+		return;
+	}
+	fout << "Year of development = " << obj->year_of_development << endl;
+}
---
+++ Tec/Language.h
@@ -0,0 +1,25 @@
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+#include "Procedural.h"
+#include "OOP.h"
+#include "Functional.h"
+
+namespace Filippov
+{
+	struct Language
+	{
+		enum lang
+		{
+			PROCEDURAL,
+			OOP,
+			FUNCTIONAL
+		} key;
+		unsigned short int year_of_development;
+	};
+
+	Language* Language_Input(ifstream &fin);
+	void Language_Output(Language *obj, ofstream &fout);
+}
+
+#endif // !LANGUAGE_H
